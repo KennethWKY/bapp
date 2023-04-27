8e5d4d5dce4578c84848b30bffc6646bb4af322c
KennethWKY <kennethwong630@gmail.com>
2023-04-27 13:28:32 -0400

    test empty index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,33 +5,33 @@ import BottomNav from "../components/BottomNav";
 import { useUser } from "@auth0/nextjs-auth0/client";
 import CreateUser from "../components/CreateUser";
 
-export async function getServerSideProps() {
-  try {
-    await clientPromise;
-    // `await clientPromise` will use the default database passed in the MONGODB_URI
-    // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
-    //
-    // `const client = await clientPromise`
-    // `const db = client.db("myDatabase")`
-    //
-    // Then you can execute queries against your database like so:
-    // db.find({}) or any of the MongoDB Node Driver commands
+// export async function getServerSideProps() {
+//   try {
+//     await clientPromise;
+//     // `await clientPromise` will use the default database passed in the MONGODB_URI
+//     // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
+//     //
+//     // `const client = await clientPromise`
+//     // `const db = client.db("myDatabase")`
+//     //
+//     // Then you can execute queries against your database like so:
+//     // db.find({}) or any of the MongoDB Node Driver commands
 
-    return {
-      props: { isConnected: true },
-    };
-  } catch (e) {
-    console.error(e);
-    return {
-      props: { isConnected: false },
-    };
-  }
-}
+//     return {
+//       props: { isConnected: true },
+//     };
+//   } catch (e) {
+//     console.error(e);
+//     return {
+//       props: { isConnected: false },
+//     };
+//   }
+// }
 
-export default function Home({
-  isConnected,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  const { user, isLoading } = useUser();
+export default function Home() {
+  //   isConnected,
+  // }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  //   const { user, isLoading } = useUser();
 
   return (
     <div>
@@ -39,16 +39,16 @@ export default function Home({
         <div className="flex flex-col max-w-md mx-auto p-6 rounded-lg shadow-lg bg-white">
           <div className="flex justify-between">
             <div className="text-3xl text-green-600">Budget Gur</div>
-            {user && (
+            {/* {user && (
               <a
                 href="/api/auth/logout"
                 className="block text-center bg-red-500 hover:bg-red-600 text-white py-2 px-4 rounded"
               >
                 Logout
               </a>
-            )}
+            )} */}
           </div>
-          {user && (
+          {/* {user && (
             <p className="text-xl text-gray-500 mt-4">Welcome {user?.name}</p>
           )}
           {!user && (
@@ -64,7 +64,7 @@ export default function Home({
               <CreateUser />
               <BottomNav />
             </div>
-          )}
+          )} */}
         </div>
       </div>
     </div>
