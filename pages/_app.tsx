87ee40c8a918fdf282735e07b52b9c799713070f
KennethWKY <kennethwong630@gmail.com>
2023-04-27 11:57:00 -0400

    Myapp fixed import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
+import { AppProps } from "next/app";
 import "../styles/global.css";
 import "tailwindcss/tailwind.css";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <UserProvider>
       <Component {...pageProps} />
